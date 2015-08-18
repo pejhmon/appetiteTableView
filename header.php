98b42c2c13f371f3487615dd8c4316296ed136df
pejhmon <pejhmon@gmail.com>
2015-08-18 18:38:07 +0100

    First

---
+++ header.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>TableView</title>
+	<meta charset='utf-8'>
+	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<!-- Bootstrap-->
+	<link href='css/bootstrap.min.css' rel='stylesheet'>
+	<link href='css/bootstrap-theme.min.css' rel='stylesheet'>
+</head>
+<body>
+	<nav class='navbar navbar-inverse navbar-fixed-top'>
+		<div class='container'>
+			<div class='navbar-header'>
+				<a class='navbar-brand' href='index.php'>Appetite Cheat Sheet</a>
+			</div>
+			<div id='navbar' class='navbar-collapse collapse'>
+				<ul class='nav navbar-nav'>
+					<li><a href='users.php'>users</a></li>
+					<li><a href='usl.php'>userSymptomList</a></li>
+					<li><a href='usm.php'>userSymptomManifest</a></li>
+					<li><a href='uwm.php'>userWeightManifest</a></li>
+					<li><a href='urm.php'>userReequirementsManifest</a></li>
+					<li><a href='uml.php'>userMealList</a></li>
+					<li><a href='ufl.php'>userFoodList</a></li>
+					<li><a href='ufm.php'>userFoodManifest</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>

b119f23abb22cba94ac2665dc545716b466c2176
pejhmon <pejhmon@gmail.com>
2015-08-18 18:41:13 +0100

    First

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php require_once 'dbconnection.php'; ?>
 <?php include 'header.php'; ?>
 
-
-<h1>Select one of the tables from the header above</h1>
-
+<div class='container'>
+    <div class='row'>
+        <h1>Select one of the tables from the header above</h1>
+    </div>
+</div>
 
 </body>
 </html>
--- users.php
+++ users.php
@@ -1,44 +1,46 @@
 <?php require_once 'dbconnection.php'; ?>
 <?php include 'header.php'; ?>
 
-<div class="row">
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>group</th>
-                <th>password</th>
-                <th>nhsnumber</th>
-                <th>dateofbirth</th>
-                <th>gender</th>
-                <th>activitylevel</th>
-                <th>registrationtimestamp</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                $query  = "SELECT * FROM users";
-                $result = sqlsrv_query($conn, $query);
-                confirm_query($result);     
-            ?>
-            <?php
-                while($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC)){
-            ?>
+<div class='container'>
+    <div class="row">
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <td><?php echo $row["id"]; ?></td>
-                    <td><?php echo $row["group"]; ?></td>
-                    <td><?php echo $row["password"]; ?></td>
-                    <td><?php echo $row["nhsnumber"]; ?></td>
-                    <td><?php echo $row["dateofbirth"]; ?></td>
-                    <td><?php echo $row["gender"]; ?></td>
-                    <td><?php echo $row["activitylevel"]; ?></td>
-                    <td><?php echo $row["registrationtimestamp"]; ?>
+                    <th>id</th>
+                    <th>group</th>
+                    <th>password</th>
+                    <th>nhsnumber</th>
+                    <th>dateofbirth</th>
+                    <th>gender</th>
+                    <th>activitylevel</th>
+                    <th>registrationtimestamp</th>
                 </tr>
-            <?php
-                }
-            ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php
+                    $query  = "SELECT * FROM users";
+                    $result = sqlsrv_query($conn, $query);
+                    confirm_query($result);     
+                ?>
+                <?php
+                    while($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC)){
+                ?>
+                    <tr>
+                        <td><?php echo $row["id"]; ?></td>
+                        <td><?php echo $row["group"]; ?></td>
+                        <td><?php echo $row["password"]; ?></td>
+                        <td><?php echo $row["nhsnumber"]; ?></td>
+                        <td><?php echo $row["dateofbirth"]; ?></td>
+                        <td><?php echo $row["gender"]; ?></td>
+                        <td><?php echo $row["activitylevel"]; ?></td>
+                        <td><?php echo $row["registrationtimestamp"]; ?>
+                    </tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </div>
 </body>
 </html>
